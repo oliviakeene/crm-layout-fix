ad590f2d5439744ebf531c747c960b91e9b0f866
Olivia Keene <olivia.keene55@gmail.com>
2023-12-08 01:02:56 +0100

    Fix styles that control the height of the grid

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
-const allowStyleOverride = window.location.href.toLowerCase().includes("insightly.com/reports");
+const allowStyleOverride = window.location.href.toLowerCase().includes("insightly.com/reports/edit");
+
+function getFirstElementByClassName(className) {
+    if(!!className) {
+        const elements = document.getElementsByClassName(className);
+        if(!!elements && elements.length > 0) {
+            return elements[0];
+        }
+    }
+}
+
+function getHeightOfElementByClassName(className) {
+    if(!!className) {
+        const element = getFirstElementByClassName(className);
+        if(!!element) {
+            return element.offsetHeight;
+        }
+    }
+}
 
 async function createStyleElement() {
     console.log("createStyleElement");
     console.log({allowStyleOverride});
     if (allowStyleOverride) {
+        const pastInfoHeight = getHeightOfElementByClassName("past-info");
+        const sidebarHeight = getHeightOfElementByClassName("sidebar-left");
+        let calculatedHeightForGrid = sidebarHeight;
+        if(!!calculatedHeightForGrid) {
+            calculatedHeightForGrid = sidebarHeight - (pastInfoHeight || 0) - 15;
+        }
+        if(!!calculatedHeightForGrid && calculatedHeightForGrid > 0) {
+            calculatedHeightForGrid += "px";
+        } else {
+            calculatedHeightForGrid = "1000vh";
+        }
         let style = document.createElement('style');
         style.type = 'text/css';
         style.textContent = `
         #main-container {
             width:1000vw!important;
         }
-        #report-editor .k-grid-content { width: 1000vh!important }
+        #grid {
+            height: ${calculatedHeightForGrid}!important;
+            max-height: ${calculatedHeightForGrid}!important;
+        }
         `;
         document.head.appendChild(style);
     }
